bb107db702a6ea45c441d52be32be7c5b3b2c2bc
Bryce Murray <brycepmurray711@gmail.com>
2018-02-16 13:52:16 -0700

    controlling the situation

--- app/game-controller.js
+++ app/game-controller.js
@@ -2,7 +2,31 @@ function GameController() {
     var gameService = new gameService()
 
 
+    function getGames() {
+        gameService.getGames(draw)
+    }
 
+    this.createGame = function createGame(e) {
+        event.preventDefault()
+        var form = event.target
+        gameService.createGame(form, draw)
+        form.reset()
+    }
+
+    this.deleteGame = function deleteGame(id) {
+        gameService.deleteGame(id, draw)
+    }
+
+    function draw(games) {
+        var template = ''
+        if (games.length < 1) {
+            carsElem.innerHTML = '<h4> Sorry No Games To Continue, To Play Start A New Game </h4>'
+            return
+        }
+        games.forEach(games => {
+            template += ``
+        })
+    }
 
 
 
